c109cddfe89d7c0119b8c3d1c08898c268c2cf4c
harrisonblackburn <blackbhr@yahoo.com>
2021-04-28 11:30:03 -0500

    attempted to fix bugs

--- team.js
+++ team.js
@@ -1,49 +1,123 @@
-// TODO: Include packages needed for this application
+
 const fs = require('fs');
-// TODO: Create an array of questions for user input
+
 const inquirer = require('inquirer');
 
-// class Team {
-//   constructor(name, id, title, email) {
-//     this.name = name;
-//     this.id = id;
-//     this.title = title;
-//     this.email = email;
-//   };
+
+const managerQuestions = [
+  {
+    type: 'input',
+    message: 'What is the team managers name?',
+    name: 'managerName',
+  }, 
+  {
+    type: 'input',
+    message: 'What is the team managers id?',
+    name: 'managerId',
+  },
+  {
+    type: 'input',
+    message: 'What is the managers email?',
+    name: 'managerEmail',
+  }, 
+  {
+    type: 'input',
+    message: 'What is the managers office number?',
+    name: 'officeNumber',
+  }
+];
+
+const engineerQuestions = [
+  {
+    type: 'input', 
+    message: 'What is the engineers name?',
+    name: 'engineerName',
+  }, 
+  {
+    type: 'input',
+    message: 'What is the engineers id?',
+    name: 'engineerId',
+  }, 
+  {
+    type: 'input', 
+    message: 'What is the engineers github username?',
+    name: 'Github'
+  }, 
+  {
+    type: 'input',
+    message: 'What is the engineers email?', 
+    name: 'engineerEmail',
+  }
+]; 
+
+const internQuestions = [
+  {
+    type: 'input',
+    message: 'What is the interns name?',
+    name: 'internName',
+  },
+  {
+    type: 'input',
+    message: 'What is the interns id?',
+    name: 'internId',
+  },
+  {
+    type: 'input',
+    message: 'What is the interns email?',
+    name: 'internEmail',
+  },
+  {
+    type: 'input',
+    message: 'Which school did the intern attend?',
+    name: 'School',
+  },
+];
+
+function init() {
+  inquirer
+    .prompt(managerQuestions)
+    .then(data => {
+      console.log(data); 
+      const manager = new Manager(data.managerName, data.managerId, data.managerEmail, data.officeNumber)
+      employees.push(manager);
+      
+    }
+      )
+}
 
 
 
-inquirer
-  .prompt([
-    {
-      type: 'input',
-      message: 'What is the name of this team member?',
-      name: '#Name',
-    },
-    {
-      type: 'list',
-      choices: ['Manager', 'Engineer', 'Intern'],
-      message: 'What is his or her job title?',
-      name: '##JobTitle',
-    },
-    {
-      type: 'input',
-      message: 'What is the id for this team member?',
-      name: '##Id',
-    },
+// inquirer
+//   .prompt([
+//     {
+//       type: 'input',
+//       message: 'What is the name of this team member?',
+//       name: '#Name',
+//     },
+//     {
+//       type: 'list',
+//       choices: ['Manager', 'Engineer', 'Intern'],
+//       message: 'What is his or her job title?',
+//       name: '##JobTitle',
+//     },
+//     {
+//       type: 'input',
+//       message: 'What is the id for this team member?',
+//       name: '##Id',
+//     },
     
-    {
-     type: 'input',
-     validate: function(input){
+//     {
+//      type: 'input',
+//      validate: function(input){
          
-         if (input.length > 6) return true;
-         else return "Must be a valid email"
-     },
-     message: 'What is this email for this team member?',
-     name: '##Email',
-    },
-  ])
+//          if (input.length > 6) return true;
+//          else return "Must be a valid email"
+//      },
+//      message: 'What is this email for this team member?',
+//      name: '##Email',
+//     },
+//   ])
 
-  .then((response) =>{
-    console.log(JSON.stringify(response, null, 2));
-});
+//   .then((response) =>{
+//     console.log(JSON.stringify(response, null, 2));
+// });
